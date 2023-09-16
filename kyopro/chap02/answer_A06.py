69c7a02c208d8cb12b5461bfb369783aea085634
Junya-0220 <junya010220@gmail.com>
2023-09-16 14:47:09 +0900

    累積和

--- kyopro/chap02/answer_A06.py
+++ kyopro/chap02/answer_A06.py
@@ -1,6 +1,11 @@
+"""
+累積和を求めてから、対象の期間の累計を出力する。
+
+"""
+
 N, Q = map(int, input().split())
 A = list(map(int, input().split()))
-L = [ None ] * Q
+L = [ None ] * Q # Noneという要素を初期化で入れている
 R = [ None ] * Q
 for j in range(Q):
 	L[j], R[j] = map(int, input().split())
@@ -12,3 +17,11 @@ for i in range(N):
  
 for j in range(Q):
 	print(S[R[j]] - S[L[j] - 1])
+
+# ライブラリを使って計算するとこんな感じ
+
+import itertools
+
+original_list = [11, 46, 47, 77, 80]
+cumulative_sum = list(itertools.accumulate(original_list))
+print(cumulative_sum)
--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-# 入力
-N = int(input())
+import itertools
 
-# 上の桁から順番に「2 進法に変換した値」を求める
-l = [i for i in range(9,-1,-1)] #[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-# [512,256,128,64,32,16,8,4,2,1] これで割る
-# print(l)
-for x in l:
-	wari = (2 ** x)
-	print((N // wari) % 2, end='')
-
-# 最後に改行する
-# print("")
-print(90 // 512 % 2)
-print(90 // 256 % 2)
-print(90 // 128 % 2)
-print(90 // 64 % 2)
-print(90 // 32 % 2)
-print(90 // 16 % 2)
-print(90 // 8 % 2)
-print(90 // 4 % 2)
-print(90 // 2 % 2)
-print(90 // 1 % 2)
+original_list = [11, 46, 47, 77, 80]
+cumulative_sum = list(itertools.accumulate(original_list))
+print(cumulative_sum)
